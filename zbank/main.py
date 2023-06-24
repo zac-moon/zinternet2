bbb6a5b549ea6609ec9c4859ebe4d9581a04952f
zac-moon <zacemoon@gmail.com>
2023-06-24 12:24:51 +0100

    fixed login method

--- zbank/main.py
+++ zbank/main.py
@@ -10,10 +10,17 @@ usercodeEntry = tk.Entry(root)
 accountNumberLabel = tk.Label(root, text="Account Number:",font=('Arial',20))
 accountNumberEntry = tk.Entry(root)
 
+def bankMenu(fullName):
+    print("Bank Menu")
+    client = tk.Toplevel()
+    client.title("ZBANK- "+fullName)
 
 def login():
-    usercode
-    print(f"Login- {}")
+    usercodeTry =  usercodeEntry.get()
+    accountNumberTry = accountNumberEntry.get()
+    print(f"Login- {usercodeTry} - {accountNumberTry}")
+    if usercodeTry == "zacmoon" and accountNumberTry =="436118":
+        bankMenu("Zac Moon")
 
 
 loginBtn = tk.Button(root, text="Login",command=login)
